a849c0c05660b5c83a9f7c89caccea761ddc876a
virean196 <gama.bruno.196@gmail.com>
2025-08-25 19:44:37 +0100

    Initial commit + basic file server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/virean196/chirpy
+
+go 1.24.5
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	httpServer := http.Server{
+		Handler: mux,
+		Addr:    ":8080",
+	}
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	httpServer.ListenAndServe()
+}

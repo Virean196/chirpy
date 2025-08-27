6225701ed0b182c01bacdae60c38a49f2b68b6c0
virean196 <gama.bruno.196@gmail.com>
2025-08-27 10:43:30 +0100

    adding validation to post req

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -47,11 +48,63 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(metrics))
 	})
+
+	// Validate Chirpy JSON POST
+	mux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, req *http.Request) {
+		type params struct {
+			Body string `json:"body"`
+		}
+		type validResp struct {
+			Valid bool `json:"valid"`
+		}
+		type invalidResp struct {
+			Error string `json:"error"`
+		}
+
+		dec := json.NewDecoder(req.Body)
+		par := params{}
+		err := dec.Decode(&par)
+		if err != nil {
+			log.Printf("error decoding parameters: %s", err)
+			resp := invalidResp{Error: "Something went wrong"}
+			dat, err := json.Marshal(resp)
+			if err != nil {
+				log.Printf("error marshalling response: %s", err)
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(500)
+			w.Write(dat)
+			return
+		}
+		if len(par.Body) <= 140 {
+			resp := validResp{Valid: true}
+			dat, err := json.Marshal(resp)
+			if err != nil {
+				log.Printf("error marshalling response: %s", err)
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(200)
+			w.Write(dat)
+
+		} else {
+			resp := invalidResp{Error: "Chirp is too long"}
+			dat, err := json.Marshal(resp)
+			if err != nil {
+				log.Printf("error marshalling response: %s", err)
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(400)
+			w.Write(dat)
+		}
+	})
+
 	// Reset metrics handle
 	mux.HandleFunc("POST /admin/reset", func(w http.ResponseWriter, req *http.Request) {
 		apiConfig.fileserverHits.Store(0)
 		w.WriteHeader(http.StatusOK)
 	})
+
+	// Start server
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(httpServer.ListenAndServe())
 }
